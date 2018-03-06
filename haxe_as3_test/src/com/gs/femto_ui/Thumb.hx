8e7333fce4df9d7a5b169349eef57204e903e7c2
Oleg Bulychov <oleg.bulychov@gmail.com>
2018-03-06 03:47:11 +0300

    +simple scrollbar

---
+++ haxe_as3_test/src/com/gs/femto_ui/Thumb.hx
@@ -0,0 +1,149 @@
+package com.gs.femto_ui;
+
+import flash.Lib;
+import flash.display.DisplayObjectContainer;
+import flash.events.MouseEvent;
+import flash.geom.Rectangle;
+
+class Thumb extends Visel
+{
+	public var is_drag_mode(get, never) : Bool;
+
+	private var drag_rect_ : Rectangle;
+
+	public function new(owner : Scrollbar)
+	{
+		super(owner);
+		create_Listeners();
+	}
+	//.............................................................................
+	private function create_Listeners() : Void
+	{
+		addEventListener(MouseEvent.ROLL_OVER, on_Mouse_Over, false, 0, true);
+		addEventListener(MouseEvent.MOUSE_DOWN, on_Mouse_Down, false, 0, true);
+	}
+	//.............................................................................
+	private function on_Mouse_Down(e : MouseEvent) : Void
+	{
+		if ((state_ & Visel.STATE_DRAG) != 0)
+		{
+			return;
+		}
+		var p : Scrollbar = Lib.as(parent, Scrollbar);
+		var nh : Float = Math.floor(p.height - height_);
+		if (nh <= 0)
+		{
+			return;
+		}  //:unable to drag
+		state_ |= Visel.STATE_DRAG;
+		e.stopPropagation();
+		invalidate(Visel.INVALIDATION_FLAG_STATE);
+		stage.addEventListener(MouseEvent.MOUSE_UP, on_Mouse_Up_Stage, false, 1);  //:set capture
+		stage.addEventListener(MouseEvent.MOUSE_MOVE, on_Mouse_Move_Stage, false, 1);
+		if (null == drag_rect_)
+		{
+			drag_rect_ = new Rectangle(0, 0, 0, nh);
+		}
+		else
+		{
+			drag_rect_.height = nh;
+		}
+		startDrag(false, drag_rect_);
+	}
+	//.............................................................................
+	private function on_Mouse_Up_Stage(e : MouseEvent) : Void
+	//trace("thumb::mouse up");
+	{
+
+		stage.removeEventListener(MouseEvent.MOUSE_UP, on_Mouse_Up_Stage);
+		if ((state_ & Visel.STATE_DRAG) != 0)
+		{
+			state_ &= ~Visel.STATE_DRAG;
+			stopDrag();
+			invalidate(Visel.INVALIDATION_FLAG_STATE);
+			var p : Scrollbar = Lib.as(parent, Scrollbar);
+			if (p != null)
+			{
+				p.on_Thumb_Finish_Drag();
+			}
+		}
+	}
+	//.............................................................................
+	private function on_Mouse_Move_Stage(e : MouseEvent) : Void
+	//trace("thumb::mouse move");
+	{
+		if ((state_ & Visel.STATE_DRAG) != 0)
+		{
+			var p : Scrollbar = Lib.as(parent, Scrollbar);
+			if (p != null)
+			{
+				e.updateAfterEvent();
+				//trace("thumb::do drag");
+				p.on_Thumb_Do_Drag();
+				return;
+			}
+		}
+		stage.removeEventListener(MouseEvent.MOUSE_MOVE, on_Mouse_Move_Stage);
+	}
+	//.............................................................................
+	//.............................................................................
+	//.............................................................................
+	//.............................................................................
+	private function on_Mouse_Over(e : MouseEvent) : Void
+	{
+		if ((state_ & Visel.STATE_HOVER) != 0)
+		{
+			return;
+		}
+		state_ |= Visel.STATE_HOVER;
+		invalidate(Visel.INVALIDATION_FLAG_STATE);
+		addEventListener(MouseEvent.ROLL_OUT, on_Mouse_Out, false, 0, true);
+	}
+	//.............................................................................
+	private function on_Mouse_Out(e : MouseEvent) : Void
+	{
+		state_ &= ~Visel.STATE_HOVER;
+		invalidate(Visel.INVALIDATION_FLAG_STATE);
+		removeEventListener(MouseEvent.ROLL_OUT, on_Mouse_Out);
+	}
+	//.............................................................................
+	//.............................................................................
+	//.............................................................................
+	override public function draw() : Void
+	{
+		if ((invalid_flags_ & (Visel.INVALIDATION_FLAG_SKIN | Visel.INVALIDATION_FLAG_SIZE | Visel.INVALIDATION_FLAG_STATE)) != 0)
+		{
+			graphics.clear();
+			if (dummy_alpha_ >= 0)
+			{
+				var r : Root = Root.instance;
+				var cl : Int = dummy_color_;
+				var frame : Int = 0;
+				if ((state_ & Visel.STATE_DISABLED) != 0)
+				{
+					cl = r.color_disabled_;
+				}
+				else
+				{
+					if ((state_ & Visel.STATE_DRAG) != 0)
+					{
+						cl = r.color_pressed_;
+					}
+					if ((state_ & Visel.STATE_HOVER) != 0)
+					{
+						frame = r.hover_inflation_;
+					}
+				}
+				graphics.beginFill(cl, dummy_alpha_);
+				graphics.drawRect(-frame, -frame, width_ + 2 * frame, height_ + 2 * frame);
+				graphics.endFill();
+			}
+		}
+	}
+	//.............................................................................
+	//.............................................................................
+	private function get_is_drag_mode() : Bool
+	{
+		return (state_ & Visel.STATE_DRAG) != 0;
+	}
+}
